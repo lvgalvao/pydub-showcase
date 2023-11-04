4d355f57f6318067d00eb649e24b12cc9d23900a
Luciano Filho <lvgalvaofilho@gmail.com>
2023-11-04 05:21:54 -0300

    add case_00

---
+++ cases/case_00.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+from pydub import AudioSegment
+
+# Define the base assets directory
+assets_dir = Path("assets")
+
+# Define the source and target file paths
+bass_wav_path = assets_dir / "bass.wav"
+bass_mp3_path = assets_dir / "bass.mp3"
+
+# Load the audio file
+bass = AudioSegment.from_file(bass_wav_path)
+
+# Export the audio file to a different format
+bass.export(bass_mp3_path, format="mp3")
